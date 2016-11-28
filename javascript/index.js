77398c2fc0e5af3555a6b85888893025cab79fdb
vijay22uk <vijay22uk@hotmail.com>
2016-11-29 00:20:15 +0530

    colors

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $('#fullpage').removeClass("hidden");
     $('#fullpage').fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f7f7f7']
+        loopBottom:true,
+        sectionsColor:["#f7f7f7", "#1bbc9b", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#7BAABE", "#f7f7f7"]
+
     });
 });
