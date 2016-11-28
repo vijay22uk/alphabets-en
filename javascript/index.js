f0955ae8cfa9d1e65fd87d2460fd703b1bdc052b
vijay22uk <vijay22uk@hotmail.com>
2016-11-29 01:01:19 +0530

    random colors

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#fullpage').removeClass("hidden");
     $('#fullpage').fullpage({
         loopBottom:true,
-        sectionsColor:["#f7f7f7", "#1bbc9b", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#1bbc9b", "#4BBFC3", "#7BAABE", "#f7f7f7", "#7BAABE", "#f7f7f7"]
+        sectionsColor:["#f7f7f7", "#cdcecd", "#f7f7f7", "#f7f7f7", "#cdcecd", "#6600cc", "#f7f7f7", "#f7f7f7", "#cdcecd", "#6600cc", "#f7f7f7", "#f7f7f7", "#cdcecd", "#6600cc", "#f7f7f7", "#f7f7f7", "#cdcecd", "#6600cc", "#f7f7f7", "#f7f7f7", "#cdcecd", "#6600cc", "#f7f7f7", "#f7f7f7", "#f7f7f7", "#f7f7f7"]
 
     });
 });
